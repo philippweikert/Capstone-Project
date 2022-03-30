9359d7185c477ea58aa3e179b2ac33b9e96ace32
philipp.weikert <philippweikert3@gmail.com>
2022-03-30 15:39:46 +0200

    userlogintests2

--- backend/src/main/java/de/neuefische/capstone/user/create/UserRepo.java
+++ backend/src/main/java/de/neuefische/capstone/user/create/UserRepo.java
@@ -1,6 +1,5 @@
 package de.neuefische.capstone.user.create;
 
-import de.neuefische.capstone.user.create.AppUser;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
