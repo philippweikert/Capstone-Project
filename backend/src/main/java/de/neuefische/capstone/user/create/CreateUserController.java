8463842ecffb4c95dfe9f2ada277e6241b6e118e
philipp.weikert <philippweikert3@gmail.com>
2022-04-04 17:33:20 +0200

    mit änderung

--- backend/src/main/java/de/neuefische/capstone/user/create/CreateUserController.java
+++ backend/src/main/java/de/neuefische/capstone/user/create/CreateUserController.java
@@ -2,7 +2,6 @@ package de.neuefische.capstone.user.create;
 
 import de.neuefische.capstone.user.login.CreateUserCredentials;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -13,14 +12,13 @@ import org.springframework.web.bind.annotation.*;
 public class CreateUserController {
 
     private final UserService userService;
-    private final PasswordEncoder passwordEncoder;
 
     @PostMapping
     public AppUser createUser (@RequestBody CreateUserCredentials createCredentials) {
         if(!createCredentials.getPassword().equals(createCredentials.getRepeatPassword())){
             throw new IllegalArgumentException("Passwörter stimmen nicht überein");
         }
-        createCredentials.setPassword(passwordEncoder.encode(createCredentials.getPassword()));
+        createCredentials.setPassword(createCredentials.getPassword());
         return userService.createUser(createCredentials);
     }
 
